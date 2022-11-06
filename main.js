f03dc7120c7f79b71316470e573d17656ee793e6
Miller <guilhermemillher@outlook.com>
2022-11-06 12:28:59 -0200

    update cards

--- main.js
+++ main.js
@@ -27,4 +27,6 @@ document.querySelector('#cards').innerHTML =
 
         createCard('24/11', 'quinta', createGame("brazil", "16:00", "serbia")) +
         createCard('28/11', 'segunda', createGame("switzerland", "13:00", "brazil") + createGame("portugal", "16:00", "uruguay")) +
-        createCard('02/12', 'sexta', createGame("brazil", "16:00", "cameroon")) 
+        createCard('02/12', 'sexta', createGame("brazil", "16:00", "cameroon")) +
+        createCard('20/11', 'domingo', createGame("qatar", "13:00", "ecuador")) + 
+        createCard('21/11', 'segunda', createGame("england", "10:00", "iran") + createGame("senegal", "13:00", "netherlands") + createGame("unitedstates", "16:00", "wales"))
