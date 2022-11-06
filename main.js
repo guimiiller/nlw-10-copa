2eaa476825d7de7833f2d1a77ad05da0590d0c0f
Miller <guilhermemillher@outlook.com>
2022-11-06 12:58:02 -0200

    card finish

--- main.js
+++ main.js
@@ -29,4 +29,5 @@ document.querySelector('#cards').innerHTML =
         createCard('28/11', 'segunda', createGame("switzerland", "13:00", "brazil") + createGame("portugal", "16:00", "uruguay")) +
         createCard('02/12', 'sexta', createGame("brazil", "16:00", "cameroon")) +
         createCard('20/11', 'domingo', createGame("qatar", "13:00", "ecuador")) + 
-        createCard('21/11', 'segunda', createGame("england", "10:00", "iran") + createGame("senegal", "13:00", "netherlands") + createGame("unitedstates", "16:00", "wales"))
+        createCard('21/11', 'segunda', createGame("england", "10:00", "iran") + createGame("senegal", "13:00", "netherlands") + createGame("unitedstates", "16:00", "wales")) +
+        createCard('22/11', 'terça', createGame("argentina", "07:00", "saudiarabia") + createGame("denmark", "10:00", "tunisia") + createGame("mexico", "13:00", "poland"))
